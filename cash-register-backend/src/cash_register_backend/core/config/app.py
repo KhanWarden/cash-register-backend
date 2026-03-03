dbcadc68366fd111dfa3fa0769963b0d3fb667c7
KhanWarden <baryrhan2004@gmail.com>
2026-03-04 03:04:43 +0500

    Add reload parameter for app

--- cash-register-backend/src/cash_register_backend/core/config/app.py
+++ cash-register-backend/src/cash_register_backend/core/config/app.py
@@ -5,3 +5,4 @@ class AppConfig(BaseModel):
     title: str = "Cash Register Backend"
     host: str = "0.0.0.0"
     port: int = 8000
+    reload: bool = False
--- cash-register-backend/src/cash_register_backend/main.py
+++ cash-register-backend/src/cash_register_backend/main.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
         app,
         host=settings.app.host,
         port=settings.app.port,
+        reload=settings.app.reload,
     )
