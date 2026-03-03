48256e61b1b6fc4e1c55365f2486ea21c93ae7a5
KhanWarden <baryrhan2004@gmail.com>
2026-03-04 03:04:01 +0500

    Create main.py for application start

---
+++ cash-register-backend/src/cash_register_backend/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+
+from cash_register_backend.app import app
+from cash_register_backend.core.config import settings
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host=settings.app.host,
+        port=settings.app.port,
+    )
