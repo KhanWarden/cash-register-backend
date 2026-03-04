f548a2ba5ccc4efc1a64df0feb24be03c0520360
KhanWarden <baryrhan2004@gmail.com>
2026-03-05 01:41:45 +0500

    Create password hasher

---
+++ cash-register-backend/src/cash_register_backend/infrastructure/security/password_hasher.py
@@ -0,0 +1,21 @@
+from passlib.context import CryptContext
+
+from cash_register_backend.domain.user import IPasswordService
+
+
+class BcryptPasswordService(IPasswordService):
+    def __init__(self) -> None:
+        self._context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def hash(
+        self,
+        plain_password: str,
+    ) -> str:
+        return self._context.hash(plain_password)
+
+    def verify(
+        self,
+        plain_password: str,
+        hashed_password: str,
+    ) -> bool:
+        return self._context.verify(plain_password, hashed_password)
