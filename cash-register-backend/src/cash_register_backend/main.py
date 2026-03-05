71246457c5c640357f19755edeef3cf033ca7ac8
KhanWarden <baryrhan2004@gmail.com>
2026-03-06 02:37:46 +0500

    Fix application start as uvicorn requires

--- cash-register-backend/src/cash_register_backend/main.py
+++ cash-register-backend/src/cash_register_backend/main.py
@@ -1,11 +1,10 @@
 import uvicorn
 
-from cash_register_backend.app import app
 from cash_register_backend.core.config import settings
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "app:app",
         host=settings.app.host,
         port=settings.app.port,
         reload=settings.app.reload,
