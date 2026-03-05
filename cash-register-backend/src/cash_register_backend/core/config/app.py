5a2992c0319ba1866cd767235a6aace1bd08f5d9
KhanWarden <baryrhan2004@gmail.com>
2026-03-06 03:01:36 +0500

    Configurate currency in app config

--- cash-register-backend/src/cash_register_backend/core/config/app.py
+++ cash-register-backend/src/cash_register_backend/core/config/app.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
 
+from cash_register_backend.core.config.currency_enum import Currency
+
 
 class AppConfig(BaseModel):
     title: str = "Cash Register Backend"
     host: str = "0.0.0.0"
     port: int = 8000
     reload: bool = False
+
+    currency: Currency
