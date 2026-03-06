98bcdb8e7be3ff12cdbcedf00e05d13d6ec5c683
KhanWarden <baryrhan2004@gmail.com>
2026-03-07 02:01:43 +0500

    Delete unnecessary params

--- cash-register-backend/src/cash_register_backend/infrastructure/database/models/user.py
+++ cash-register-backend/src/cash_register_backend/infrastructure/database/models/user.py
@@ -12,10 +12,10 @@ from cash_register_backend.infrastructure.database.models import Base
 
 class User(Base):
     id: Mapped[UUID] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    role: Mapped[str] = mapped_column(String(50), nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    username: Mapped[str] = mapped_column(String(100), unique=True)
+    first_name: Mapped[str] = mapped_column(String(100))
+    last_name: Mapped[str] = mapped_column(String(100))
+    role: Mapped[str] = mapped_column(String(50))
+    hashed_password: Mapped[str] = mapped_column(String(255))
     is_active: Mapped[bool] = mapped_column(Boolean)
     created_at: Mapped[datetime] = mapped_column(DateTime)
