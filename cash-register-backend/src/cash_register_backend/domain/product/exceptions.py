303a77a74a5b76aaff686bd9d8d3c3ecc3ff387d
KhanWarden <baryrhan2004@gmail.com>
2026-03-06 23:02:35 +0500

    Create exceptions for product domain

---
+++ cash-register-backend/src/cash_register_backend/domain/product/exceptions.py
@@ -0,0 +1,21 @@
+from cash_register_backend.domain.shared import DomainException
+
+
+class ProductIsInactiveException(DomainException):
+    pass
+
+
+class CannotChangePriceToZeroException(DomainException):
+    pass
+
+
+class ProductNotFoundException(DomainException):
+    pass
+
+
+class CannotAdjustStockForServiceException(DomainException):
+    pass
+
+
+class InsufficientStockException(DomainException):
+    pass
