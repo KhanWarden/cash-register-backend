7d1cca115ddd61e24709cf24066a5312a34e4a5f
KhanWarden <baryrhan2004@gmail.com>
2026-03-11 03:01:09 +0500

    Add new methods for payment domain interface repository

--- cash-register-backend/src/cash_register_backend/domain/payment/repository.py
+++ cash-register-backend/src/cash_register_backend/domain/payment/repository.py
@@ -6,7 +6,15 @@ from cash_register_backend.domain.shared import EntityId
 
 class IPaymentRepository(ABC):
     @abstractmethod
-    def get_by_receipt_id(self, receipt_id: EntityId) -> list[Payment]:
+    def get_by_payment_id(self, payment_id: EntityId) -> Payment | None:
+        pass
+
+    @abstractmethod
+    def get_by_receipt_id(self, receipt_id: EntityId) -> list[Payment] | None:
+        pass
+
+    @abstractmethod
+    def get_by_cashier_id(self, receipt_id: EntityId) -> list[Payment] | None:
         pass
 
     @abstractmethod
