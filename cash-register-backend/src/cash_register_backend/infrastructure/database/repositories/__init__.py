ae2651bca0473b31d612747b9acf966c7f039546
KhanWarden <baryrhan2004@gmail.com>
2026-03-11 03:51:01 +0500

    Add repositories to __init__.py

--- cash-register-backend/src/cash_register_backend/infrastructure/database/repositories/__init__.py
+++ cash-register-backend/src/cash_register_backend/infrastructure/database/repositories/__init__.py
@@ -1,6 +1,14 @@
 __all__ = (
     # Repositories
     "UserRepository",
+    "ProductRepository",
+    "CategoryRepository",
+    "PaymentRepository",
+    "ReceiptRepository",
 )
 
 from .user_repository import UserRepository
+from .product_repository import ProductRepository
+from .category_repository import CategoryRepository
+from .payment_repository import PaymentRepository
+from .receipt_repository import ReceiptRepository
